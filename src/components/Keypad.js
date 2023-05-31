75c66b7649a44a205ae1651f91e0346348781f72
Andrew MacIsaac <amacisaac95@gmail.com>
2023-05-31 10:05:55 -0600

    lab complete

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
+import React from 'react';
+
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <form onChange={()=>{console.log('Entering password...')}}>
+                 <input type="password" name="password" placeholder="Enter password..." />
+            </form> 
+        </div>
     )
 }
 
